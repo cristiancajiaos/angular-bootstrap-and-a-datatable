458ebd71154d49a1459ea130832bcaf43252e0d6
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-05-09 18:55:02 -0400

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent implements OnInit {
+  title = "bootstrap-and-a-datatable";
+
+  rows: any = [
+    { name: "Austin", gender: "Male", company: "Swimlane" },
+    { name: "Dany", gender: "Male", company: "KFC" },
+    { name: "Molly", gender: "Female", company: "Burger King" },
+  ];
+
+  columns = [
+    { prop: "name" },
+    { name: "Gender" },
+    { name: "Company" }
+  ];
+
+  ngOnInit() {}
+}
